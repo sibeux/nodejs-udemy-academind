9f3215bbe3a34a33382fd4cff942d250b07dd4e4
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-11-01 23:57:51 +0700

    33. Routing Requests

--- courses/basic-nodejs/app.js
+++ courses/basic-nodejs/app.js
@@ -1,8 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    // console.log(req.url, req.method, req.headers);
     // process.exit;
+    const url = req.url;
+    if (url === "/") {
+        res.write("<html>");
+        res.write("<head><title>Enter message</title></head>");
+        res.write(
+            "<body><form action='message' method='POST'><input type='text' name='message'>Hello from my NodeJS server</input></form></body>"
+        );
+        res.write("</html>");
+        return res.end;
+    }
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>My first page</title></head>");
