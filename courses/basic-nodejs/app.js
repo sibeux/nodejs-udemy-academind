d3bde66fe9001522e7001065f64ca3be2a8cbd71
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-11-02 00:20:16 +0700

    35. Parsing Request Bodies

--- courses/basic-nodejs/app.js
+++ courses/basic-nodejs/app.js
@@ -10,13 +10,22 @@ const server = http.createServer((req, res) => {
         res.write("<html>");
         res.write("<head><title>Enter message</title></head>");
         res.write(
-            "<body><form action='message' method='POST'><input type='text' name='message'>Hello from my NodeJS server</input></form></body>"
+            "<body><form action='message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></input></form></body>"
         );
         res.write("</html>");
         return res.end;
     }
     if (method === "POST" && url === "/message") {
-        fs.writeFileSync("message.txt", "DUMMY");
+        const body = [];
+        req.on("data", (chunck) => {
+            console.log(chunck);
+            body.push(chunck);
+        });
+        req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            fs.writeFileSync("message.txt", message);
+        });
         res.statusCode = 302;
         res.setHeader("Location", "/");
         return res.end();
