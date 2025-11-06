5359eeb11256f6f21201563a7d05cc2d14e7cad6
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-11-07 02:39:04 +0700

    68. Adding a 404 Error Page

--- courses/03-working-expressjs/app.js
+++ courses/03-working-expressjs/app.js
@@ -11,20 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-// app.use((req, res, next) => {
-//     console.log("Request received 1:", req.method, req.url);
-//     next(); // Allows the request to proceed to the next middleware
-// });
-
-// app.use("/", (req, res, next) => {
-//     console.log("This always runs for /");
-//     next();
-// });
-
-// setara degnan "/"
-// app.use((req, res, next) => {
-//     console.log("Request received 2:", req.method, req.url);
-//     res.send("<h1>Hello from Express!</h1>"); // Auto setHeaders as HTML
-// });
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page Not Found</h1>");
+});
 
 app.listen(3000);
