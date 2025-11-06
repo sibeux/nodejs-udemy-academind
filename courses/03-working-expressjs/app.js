4d7373be403ece527d3d49b34db6a1c1c689907d
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-11-07 03:13:06 +0700

    72. Returning a 404 Page

--- courses/03-working-expressjs/app.js
+++ courses/03-working-expressjs/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -8,11 +9,14 @@ const shopRoutes = require("../../routes/course-03/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin',adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page Not Found</h1>");
+    // res.status(404).send("<h1>Page Not Found</h1>");
+    res.status(404).sendFile(
+        path.join(__dirname, "..", "..", "views", "course-03", "404.html")
+    );
 });
 
 app.listen(3000);
