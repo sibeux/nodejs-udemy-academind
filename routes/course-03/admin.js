1160f0f2155ca966568182fa3004fafaf5c4a6bb
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-11-07 03:08:35 +0700

    71. Serving HTML Pages

--- routes/course-03/admin.js
+++ routes/course-03/admin.js
@@ -1,11 +1,23 @@
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-    res.send(
-        "<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
+    // res.send(
+    //     "<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
+    // );
+    res.sendFile(
+        // '../../views/course-03/add-product.html', // relative path not work here
+        path.join(
+            __dirname,
+            "..",
+            "..",
+            "views",
+            "course-03",
+            "add-product.html"
+        )
     );
 });
 
--- routes/course-03/shop.js
+++ routes/course-03/shop.js
@@ -1,9 +1,14 @@
+const path = require("path");
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.send("<h1>Hello from Express!</h1>"); // Auto setHeaders as HTML
+    // res.send("<h1>Hello from Express!</h1>"); // Auto setHeaders as HTML
+    res.sendFile(
+        // '../../views/course-03/shop.html', // relative path not work here
+        path.join(__dirname, "..", "..", "views", "course-03", "shop.html")
+    );
 });
 
 module.exports = router;
