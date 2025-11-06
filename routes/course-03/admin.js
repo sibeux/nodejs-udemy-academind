0af1230befbb3c6ab1b623286af84e2df2a85344
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-11-07 02:44:51 +0700

    69. Filtering Paths

--- routes/course-03/admin.js
+++ routes/course-03/admin.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-playlist => GET
 router.get("/add-playlist", (req, res, next) => {
     res.send(
-        "<form action='/playlist' method='POST'><input type='text' name='title'><button type='submit'>Add Playlist</button></form>"
+        "<form action='/admin/add-playlist' method='POST'><input type='text' name='title'><button type='submit'>Add Playlist</button></form>"
     );
 });
 
-router.post("/playlist", (req, res, next) => {
+// /admin/add-playlist => POST
+router.post("/add-playlist", (req, res, next) => {
     console.log("response body:", req.body);
     console.log("Playlist title:", req.body.title);
     res.redirect("/"); // Redirect to home page after form submission
