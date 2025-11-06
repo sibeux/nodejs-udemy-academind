4170d1014882f0294df81eefd0c8c9408cb22024
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-11-07 02:57:31 +0700

    70. Creating HTML Pages

--- routes/course-03/admin.js
+++ routes/course-03/admin.js
@@ -2,17 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-// /admin/add-playlist => GET
-router.get("/add-playlist", (req, res, next) => {
+// /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
     res.send(
-        "<form action='/admin/add-playlist' method='POST'><input type='text' name='title'><button type='submit'>Add Playlist</button></form>"
+        "<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
     );
 });
 
-// /admin/add-playlist => POST
-router.post("/add-playlist", (req, res, next) => {
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
     console.log("response body:", req.body);
-    console.log("Playlist title:", req.body.title);
+    console.log("product title:", req.body.title);
     res.redirect("/"); // Redirect to home page after form submission
 });
 
